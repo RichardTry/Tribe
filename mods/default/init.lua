1156c2f4f3986f914171bade856c98ded581dbb2
Richard Try <richardtrybox@protonmail.com>
2020-08-06 20:46:26 +0300

    Lua running from file

---
+++ mods/default/init.lua
@@ -0,0 +1,1 @@
+register_tile({texture = "grass.png"})
--- src/content.cpp
+++ src/content.cpp
@@ -16,26 +16,41 @@ int Content::register_tile(lua_State * L)
     if (lua_istable(L, 1))
     {
         lua_getfield(L, 1, "texture");
-        std::string s = luaL_checkstring(L, -1);
-        std::cout << s << std::endl;
+        std::string texture = luaL_checkstring(L, -1);
+        std::cout << texture << std::endl;
     }
     return 1;
 }
 
 void Content::initContent(std::string save)
 {
-    //lua experiments
-    lua_State * L = luaL_newstate();
-    lua_pushcfunction(L, Content::register_tile);
-    lua_setglobal(L, "register_tile");
-    int ok = luaL_dostring(L, "register_tile({texture = \"grass.png\"})");
-    if (ok != LUA_OK)
-        std::cout << "Lua error!\n";
-    else
-        std::cout << "Lua nice work!\n";
-    lua_Number x = lua_tonumber(L, 1);
-    std::cout << "Lua says: " << (int)x << std::endl;
-    lua_close(L);
+    std::ifstream ifmods("save/" + save + "/mods.txt");
+    if (ifmods.is_open())
+    {
+        while(!ifmods.eof())
+        {
+            std::string mod_name;
+            ifmods >> mod_name;
+            if (mod_name == "")
+            {
+                std::cout << "Warning: saving with zero mods\n";
+                break;
+            }
+
+            lua_State * L = luaL_newstate();
+            lua_pushcfunction(L, Content::register_tile);
+            lua_setglobal(L, "register_tile");
+            std::string lua_file = "mods/" + mod_name + "/init.lua";
+            int ok = luaL_dofile(L, lua_file.c_str());
+            if (ok != LUA_OK)
+                std::cout << "Lua error!\n";
+            else
+                std::cout << "Lua nice work!\n";
+            lua_close(L);
+
+        }
+    }
+    ifmods.close();
 
     std::ifstream iftex("resources/textures.list");
     if (iftex.is_open())
